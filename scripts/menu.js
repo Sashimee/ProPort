87cf2f2573168e93e600726df905b985a0bdb7e0
Alex <60114079+Sashimee@users.noreply.github.com>
2020-02-12 22:11:54 +0100

    new color design & menu bugfix

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,6 +3,7 @@
 const sideBar = document.querySelector("#sideBarWrapper");
 const sideBarButton = document.querySelector("#buttonBar");
 const menu = document.querySelector("#mainMenu");
+const main = document.querySelector("main");
 
 // Adding event listener
 
@@ -18,21 +19,25 @@ function windowResize() {
     if (window.innerWidth >= 1200) {
         menu.removeEventListener("click", closeMenu);
         menu.style.display = "flex";
+        main.style.display = "initial";
     }
     if (window.innerWidth < 1200) {
         menu.addEventListener("click", closeMenu);
         sideBarButton.addEventListener("click", openMenu);
         menu.style.display = "none";
         sideBar.style.display = "";
+        main.style.display = "initial";
     }
 }
 
 function openMenu() {
     sideBar.style.display = "none";
     menu.style.display = "flex";
+    main.style.display = "none";
 }
 
 function closeMenu() {
     sideBar.style.display = "";
     menu.style.display = "none";
+    main.style.display = "initial";
 }
