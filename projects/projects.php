2f44ca2d6ec7e8f348cb95365318d5e201838106
Sashimee <60114079+Sashimee@users.noreply.github.com>
2020-03-04 16:37:37 +0100

    update

--- projects/projects.php
+++ projects/projects.php
@@ -11,7 +11,7 @@
     <!--Link to main css-->
     <link rel="stylesheet" href="/style/main.css" />
     <!--Import of blog CSS-->
-    <link rel="stylesheet" href="/style/blog.css">
+    <link rel="stylesheet" href="/style/projects.css">
     <!--Link to font Awesome free cdn-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 </head>
@@ -19,23 +19,21 @@
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/php_include/menu.php'); ?>
     <main>
-        <div class="boxwrapper smaller">
+        <div class="boxwrapper">
             <h1>Projects</h1>
             <section class="welcome box">
                 <h2>Ajax call to a free API</h2>
-                <p>The purpose of this page is to show, on a regular basis, new ideas I've come up with.</p>
-                <p>So here is a small example of an ajax call.</p>
+                <p>Click on the button to see an example of an Ajax call to an API.</p>
                 <p class="cntr">
-                    <a href="/api-ajax-call-example/ajax.php" class="call-to-action">Here</a>
+                    <a href="/api-ajax-call-example/ajax.php" class="call-to-action">Go</a>
                 </p>
             </section>
             <section class="welcome box">
-                <h2>First design idea</h2>
-                <p>The purpose of this whole portfolio is exercising myself and use this work to showcase my abilities. For now there is HTML/CSS and some Javascript</p>
-                <p>My first idea is to design a sort of tool that can be useful and also complicated in coding as I need this to be a good recap exercice. I think about a tool you can use if you have a table, a few people and you want to have a visual of
-                    who you can place where.</p>
-                <p></p>
-                <p class="blog-date">15/02/2020</p>
+                <h2>Sorting of an array</h2>
+                <p>You can use this page to input a number array and check if it is sorted</p>
+                <p class="cntr">
+                    <a href="/projects/php/page1.php" class="call-to-action">Go</a>
+                </p>
             </section>
         </div>
     </main>
