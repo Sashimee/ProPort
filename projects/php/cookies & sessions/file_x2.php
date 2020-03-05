1b756ea0b9f829920eb7dd902c3e2cb918624980
Sashimee <60114079+Sashimee@users.noreply.github.com>
2020-03-05 15:44:11 +0100

    Update file_x2.php

--- projects/php/cookies & sessions/file_x2.php
+++ projects/php/cookies & sessions/file_x2.php
@@ -3,9 +3,6 @@
 // Open a file in writing mode, if it don't exist it'll create it
 
 $file_handle = fopen('last_access.log', 'w'); // You could use 'a' to append at the end of you file
-$file_content = time() . "\r\n";
-
-for ($i = 0; $i < 1; $i++) { //1500 new lines :)
-    fwrite($file_handle, $file_content);
-}
+$file_content = time();
+fwrite($file_handle, $file_content);
 fclose($file_handle);
