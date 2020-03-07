cb15b8b8c505f2a9db995bc6e40125b068a11a5c
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-07 19:55:30 +0100

    big rm

--- projects/php/cookies & sessions/file_x2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Open a file in writing mode, if it don't exist it'll create it
-
-$file_handle = fopen('last_access.log', 'w'); // You could use 'a' to append at the end of you file
-$file_content = time();
-fwrite($file_handle, $file_content);
-fclose($file_handle);
