b6f1e80e5a7bc6a48e582122ac2ef71013d7c200
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-09 16:20:10 +0100

    Update projects.php

--- projects/projects.php
+++ projects/projects.php
@@ -25,28 +25,28 @@
                 <h2>TooDooD</h2>
                 <p>Todo list with account creation. You need to login and you only have access to your own tasks</p>
                 <p class="cntr">
-                    <a href="/projects/TooDooD/index.php" class="call-to-action">Go</a>
+                    <a href="/projects/TooDooD/index.php" class="call-to-action">Visit project</a>
                 </p>
             </section>
             <section class="welcome box">
                 <h2>PHP Input Validation</h2>
                 <p>PHP Implementation of a subscription form.</p>
                 <p class="cntr">
-                    <a href="/projects/php/page2.php" class="call-to-action">Go</a>
+                    <a href="/projects/php/page2.php" class="call-to-action">Visit project</a>
                 </p>
             </section>
             <section class="welcome box">
                 <h2>Ajax call to a free API</h2>
                 <p>Click on the button to see an example of an Ajax call to an API.</p>
                 <p class="cntr">
-                    <a href="/api-ajax-call-example/ajax.php" class="call-to-action">Go</a>
+                    <a href="/api-ajax-call-example/ajax.php" class="call-to-action">Visit project</a>
                 </p>
             </section>
             <section class="welcome box">
                 <h2>Sorting of an array</h2>
                 <p>You can use this page to input a number array and check if it is sorted</p>
                 <p class="cntr">
-                    <a href="/projects/php/page1.php" class="call-to-action">Go</a>
+                    <a href="/projects/php/page1.php" class="call-to-action">Visit project</a>
                 </p>
             </section>
         </div>
